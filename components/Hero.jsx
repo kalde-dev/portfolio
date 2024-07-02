d41f4c04c850715313812714da7cc3c8d1e58d6a
Kurt Vonn Alde <kalde.pw@gmail.com>
2024-07-02 20:47:44 +0800

    added socials without links

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,7 +16,7 @@ import Socials from "./Socials";
 const Hero = () => {
   return (
     <section className="p-12 py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
-      <container className="mx-auto">
+      <div className="mx-auto">
         <div className="flex justify-between gap-x-8">
           <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
             <div className="text-sm uppercase font-semibold mb-6 text-primary tracking-[4px]">
@@ -38,13 +38,17 @@ const Hero = () => {
                 Download CV <Download size={18} />
               </Button>
             </div>
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconsStyles="text-foreground text-[30px] md:text-[35px] lg:text-[40] hover:text-primary transition-all"
+            />
           </div>
           <div className="hidden xl:flex relative">Image</div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />
         </div>
-      </container>
+      </div>
     </section>
   );
 };
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+import {
+  RiFacebookBoxFill,
+  RiGoogleFill,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiSteamFill,
+} from "react-icons/ri";
 
-const Socials = () => {
-  return <div>Socials</div>;
+import Link from "next/link";
+import { Icon } from "lucide-react";
+
+const icons = [
+  {
+    path: "#facebook",
+    name: <RiFacebookBoxFill />,
+  },
+  {
+    path: "#google",
+    name: <RiGoogleFill />,
+  },
+  {
+    path: "#github",
+    name: <RiGithubFill />,
+  },
+  {
+    path: "#linkein",
+    name: <RiLinkedinBoxFill />,
+  },
+  {
+    path: "#steam",
+    name: <RiSteamFill />,
+  },
+];
+
+const Socials = ({ containerStyles, iconsStyles }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Socials;
